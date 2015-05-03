dde6ffa932bb529e00a35273c297acf7c8b80963
cankurtan <cankrtn@gmail.com>
2015-05-03 22:42:19 +0300

    repository is moved to organization and negation operation is added.

---
+++ src/assignment8/MathematicalOperations.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package assignment8;
+
+/**
+ * @author Bunyamin
+ *
+ */
+public class MathematicalOperations {
+
+	/**
+
+
+
+	 * 2 number addition
+
+	 * @param num1 is the first number
+
+	 * @param num2 is the second number
+
+	 * @return num1 + num2
+
+	 */
+
+
+
+	public int addition(int num1, int num2) {
+
+
+		return num1 + num2 ;
+
+
+
+	}
+	public boolean negation(boolean x){
+		if(x == true){
+			return false;
+		else{
+			return true;
+		}
+	}
+
+}
---
+++ src/assignment8/TestMathematicalOperations.java
@@ -0,0 +1,43 @@
+package assignment8;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import assignment8.MathematicalOperations;
+
+/**
+ * 
+ */
+
+/**
+ *Test for MathematicalOperations Class
+ */
+public class TestMathematicalOperations {
+	
+
+	
+	/**
+
+	* to test general attitude of addition
+
+	*/
+
+	@Test
+	public void testAddition() {
+		MathematicalOperations t1 = new MathematicalOperations();
+		assertEquals("-1 + 6 must be 5", 5, t1.addition(-1, 6));
+
+		MathematicalOperations t2 = new MathematicalOperations();
+		assertEquals("30 + -8 must be 22", 22, t2.addition(30, -8));
+
+	}
+	public void testNegation() {
+		MathematicalOperations t1 = new MathematicalOperations();
+		assertEquals("negation of true", false, t1.negation(true));
+
+		MathematicalOperations t2 = new MathematicalOperations();
+		assertEquals("negation of false", true, t2.negation(false));
+
+	}
+}
