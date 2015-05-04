dc7e193e69ae4145f3502cb51c6e002ed40c9f4d
Tuba Topaloğlu <Tuba@Tuba-MacBook-Air.local>
2015-05-04 12:39:59 +0300

    Added the multiplication operation

--- src/assignment8/MathematicalOperations.java
+++ src/assignment8/MathematicalOperations.java
@@ -51,5 +51,8 @@ public class MathematicalOperations {
     public int division(int num1, int num2) {
 		return num1 / num2 ;
 	}
-        
+    
+    public int multiplication(int num1, int num2) {
+        return num1 * num2 ;
+    }
 }
