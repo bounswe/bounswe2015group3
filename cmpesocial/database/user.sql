ac5993eae5ca0f0eabd7f90403c168eb15da4e38
Umut Afacan <umut-afacan@hotmail.com>
2015-10-20 17:39:34 +0300

    sql typo fix.

--- cmpesocial/database/user.sql
+++ cmpesocial/database/user.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `surnameuser` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `surname` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `email` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `password` char(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
