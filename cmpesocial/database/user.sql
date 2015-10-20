dc2b3d35c1b9d01d0c8e7f2c8bfb7817f5587c31
can.kurtan <cankrtn@gmail.com>
2015-10-20 16:14:29 +0300

    user table sql is added

---
+++ cmpesocial/database/user.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `surnameuser` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `email` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `password` char(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
