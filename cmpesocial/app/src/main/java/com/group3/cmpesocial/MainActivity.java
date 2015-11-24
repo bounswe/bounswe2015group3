e65a65b44c162f9b48c2c273ceebb0a3c5f61533
Tuba Topaloğlu <Tuba@Tuba-MacBook-Air.local>
2015-11-24 22:14:42 +0200

    no message

--- cmpesocial/app/src/main/java/com/group3/cmpesocial/MainActivity.java
+++ cmpesocial/app/src/main/java/com/group3/cmpesocial/MainActivity.java
@@ -118,22 +118,6 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
