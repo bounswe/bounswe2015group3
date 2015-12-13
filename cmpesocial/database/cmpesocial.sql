cf66dfca920f748de59bf20179ed55436b12171c
Umut Afacan <umut-afacan@hotmail.com>
2015-12-13 12:39:19 +0200

    #227

--- cmpesocial/database/cmpesocial.sql
+++ cmpesocial/database/cmpesocial.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.10
+-- version 4.2.7
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Dec 13, 2015 at 02:57 AM
--- Server version: 5.5.42
--- PHP Version: 5.6.10
+-- Generation Time: Dec 13, 2015 at 10:37 AM
+-- Server version: 5.5.41-log
+-- PHP Version: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `cmpesocial`
@@ -20,14 +26,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `comment_event`
 --
 
-CREATE TABLE `comment_event` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `comment_event` (
+`id` int(11) NOT NULL,
   `id_post` int(11) NOT NULL,
   `id_event` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `content` varchar(500) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `comment_event`
@@ -43,7 +49,7 @@ INSERT INTO `comment_event` (`id`, `id_post`, `id_event`, `id_user`, `date`, `co
 -- Table structure for table `comment_group`
 --
 
-CREATE TABLE `comment_group` (
+CREATE TABLE IF NOT EXISTS `comment_group` (
   `id` int(11) NOT NULL,
   `id_post` int(11) NOT NULL,
   `id_group` int(11) NOT NULL,
@@ -58,17 +64,17 @@ CREATE TABLE `comment_group` (
 -- Table structure for table `event`
 --
 
-CREATE TABLE `event` (
-  `id` int(11) NOT NULL,
-  `name` varchar(64) COLLATE utf8_bin NOT NULL,
+CREATE TABLE IF NOT EXISTS `event` (
+`id` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_bin NOT NULL,
   `date` datetime NOT NULL,
   `end_date` datetime NOT NULL,
   `periodic` int(11) NOT NULL DEFAULT '0',
   `date_of_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `id_user` int(11) NOT NULL,
-  `location` varchar(64) COLLATE utf8_bin NOT NULL,
+  `location` varchar(500) COLLATE utf8_bin NOT NULL,
   `description` text COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `event`
@@ -87,15 +93,15 @@ INSERT INTO `event` (`id`, `name`, `date`, `end_date`, `periodic`, `date_of_crea
 -- Table structure for table `group`
 --
 
-CREATE TABLE `group` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) COLLATE utf8_bin NOT NULL,
+CREATE TABLE IF NOT EXISTS `group` (
+`id` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_bin NOT NULL,
   `date_of_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `id_admin` int(11) NOT NULL,
   `type` varchar(20) COLLATE utf8_bin NOT NULL,
-  `description` varchar(50) COLLATE utf8_bin NOT NULL,
+  `description` varchar(1000) COLLATE utf8_bin NOT NULL,
   `group_url` varchar(500) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=14 ;
 
 --
 -- Dumping data for table `group`
@@ -112,14 +118,14 @@ INSERT INTO `group` (`id`, `name`, `date_of_creation`, `id_admin`, `type`, `desc
 -- Table structure for table `post_event`
 --
 
-CREATE TABLE `post_event` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `post_event` (
+`id` int(11) NOT NULL,
   `id_event` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `content` varchar(500) COLLATE utf8_bin NOT NULL,
-  `content_url` varchar(200) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `content_url` varchar(1000) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `post_event`
@@ -135,14 +141,14 @@ INSERT INTO `post_event` (`id`, `id_event`, `id_user`, `date`, `content`, `conte
 -- Table structure for table `post_group`
 --
 
-CREATE TABLE `post_group` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `post_group` (
+`id` int(11) NOT NULL,
   `id_group` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `post_text` varchar(1000) COLLATE utf8_bin NOT NULL,
   `post_url` varchar(200) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `post_group`
@@ -159,8 +165,8 @@ INSERT INTO `post_group` (`id`, `id_group`, `id_user`, `date`, `post_text`, `pos
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+`id` int(11) NOT NULL,
   `name` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `surname` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `email` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
@@ -168,7 +174,7 @@ CREATE TABLE `user` (
   `date_of_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `profile_pic_link` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
   `type` varchar(20) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `user`
@@ -186,7 +192,7 @@ INSERT INTO `user` (`id`, `name`, `surname`, `email`, `password`, `date_of_creat
 -- Table structure for table `user_event`
 --
 
-CREATE TABLE `user_event` (
+CREATE TABLE IF NOT EXISTS `user_event` (
   `id_event` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `status` int(11) NOT NULL
@@ -206,7 +212,7 @@ INSERT INTO `user_event` (`id_event`, `id_user`, `status`) VALUES
 -- Table structure for table `user_group`
 --
 
-CREATE TABLE `user_group` (
+CREATE TABLE IF NOT EXISTS `user_group` (
   `id_user` int(11) NOT NULL,
   `id_group` int(11) NOT NULL,
   `status` int(11) NOT NULL
@@ -227,50 +233,49 @@ INSERT INTO `user_group` (`id_user`, `id_group`, `status`) VALUES
 -- Indexes for table `comment_event`
 --
 ALTER TABLE `comment_event`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `event`
 --
 ALTER TABLE `event`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `group`
 --
 ALTER TABLE `group`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `post_event`
 --
 ALTER TABLE `post_event`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `post_group`
 --
 ALTER TABLE `post_group`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user_event`
 --
 ALTER TABLE `user_event`
-  ADD PRIMARY KEY (`id_user`,`id_event`),
-  ADD KEY `fkevent` (`id_event`);
+ ADD PRIMARY KEY (`id_user`,`id_event`), ADD KEY `fkevent` (`id_event`);
 
 --
 -- Indexes for table `user_group`
 --
 ALTER TABLE `user_group`
-  ADD PRIMARY KEY (`id_user`,`id_group`);
+ ADD PRIMARY KEY (`id_user`,`id_group`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -280,32 +285,32 @@ ALTER TABLE `user_group`
 -- AUTO_INCREMENT for table `comment_event`
 --
 ALTER TABLE `comment_event`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `event`
 --
 ALTER TABLE `event`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `group`
 --
 ALTER TABLE `group`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `post_event`
 --
 ALTER TABLE `post_event`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `post_group`
 --
 ALTER TABLE `post_group`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- Constraints for dumped tables
 --
@@ -314,19 +319,23 @@ ALTER TABLE `user`
 -- Constraints for table `user_event`
 --
 ALTER TABLE `user_event`
-  ADD CONSTRAINT `fkevent` FOREIGN KEY (`id_event`) REFERENCES `event` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fkuser` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `fkevent` FOREIGN KEY (`id_event`) REFERENCES `event` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `fkuser` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 DELIMITER $$
 --
 -- Events
 --
-CREATE DEFINER=`root`@`localhost` EVENT `daily_event` ON SCHEDULE EVERY 5 MINUTE STARTS '2015-11-28 17:36:21' ON COMPLETION NOT PRESERVE ENABLE DO UPDATE `event`  SET `event`.`date` = DATE_ADD(`event`.`date`,INTERVAL 1 DAY) , `event`.`end_date` = DATE_ADD(`event`.`end_date`,INTERVAL 1 DAY)  WHERE `event`.`end_date`< NOW() AND `event`.`periodic` = 1$$
-
-CREATE DEFINER=`root`@`localhost` EVENT `weekly` ON SCHEDULE EVERY 5 MINUTE STARTS '2015-11-28 17:37:12' ON COMPLETION NOT PRESERVE ENABLE DO UPDATE `event`  SET `event`.`date` = DATE_ADD(`event`.`date`,INTERVAL 1 WEEK), `event`.`end_date` = DATE_ADD(`event`.`end_date`,INTERVAL 1 WEEK)  WHERE `event`.`end_date`< NOW() AND `event`.`periodic` = 2$$
+CREATE DEFINER=`root`@`localhost` EVENT `annually_events` ON SCHEDULE EVERY 5 MINUTE STARTS '2015-11-28 17:38:24' ON COMPLETION NOT PRESERVE ENABLE DO UPDATE `event`  SET `event`.`date` = DATE_ADD(`event`.`date`,INTERVAL 1 YEAR) , `event`.`end_date` = DATE_ADD(`event`.`end_date`,INTERVAL 1 YEAR) WHERE `event`.`end_date`< NOW() AND `event`.`periodic` = 4$$
 
 CREATE DEFINER=`root`@`localhost` EVENT `monthly_event` ON SCHEDULE EVERY 5 MINUTE STARTS '2015-11-28 17:37:49' ON COMPLETION NOT PRESERVE ENABLE DO UPDATE `event`  SET `event`.`date` = DATE_ADD(`event`.`date`,INTERVAL 1 MONTH) , `event`.`end_date` = DATE_ADD(`event`.`end_date`,INTERVAL 1 MONTH) WHERE `event`.`end_date`< NOW() AND `event`.`periodic` = 3$$
 
-CREATE DEFINER=`root`@`localhost` EVENT `annually_events` ON SCHEDULE EVERY 5 MINUTE STARTS '2015-11-28 17:38:24' ON COMPLETION NOT PRESERVE ENABLE DO UPDATE `event`  SET `event`.`date` = DATE_ADD(`event`.`date`,INTERVAL 1 YEAR) , `event`.`end_date` = DATE_ADD(`event`.`end_date`,INTERVAL 1 YEAR) WHERE `event`.`end_date`< NOW() AND `event`.`periodic` = 4$$
+CREATE DEFINER=`root`@`localhost` EVENT `weekly` ON SCHEDULE EVERY 5 MINUTE STARTS '2015-11-28 17:37:12' ON COMPLETION NOT PRESERVE ENABLE DO UPDATE `event`  SET `event`.`date` = DATE_ADD(`event`.`date`,INTERVAL 1 WEEK), `event`.`end_date` = DATE_ADD(`event`.`end_date`,INTERVAL 1 WEEK)  WHERE `event`.`end_date`< NOW() AND `event`.`periodic` = 2$$
+
+CREATE DEFINER=`root`@`localhost` EVENT `daily_event` ON SCHEDULE EVERY 5 MINUTE STARTS '2015-11-28 17:36:21' ON COMPLETION NOT PRESERVE ENABLE DO UPDATE `event`  SET `event`.`date` = DATE_ADD(`event`.`date`,INTERVAL 1 DAY) , `event`.`end_date` = DATE_ADD(`event`.`end_date`,INTERVAL 1 DAY)  WHERE `event`.`end_date`< NOW() AND `event`.`periodic` = 1$$
 
 DELIMITER ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
